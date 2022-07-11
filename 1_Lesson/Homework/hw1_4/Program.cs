0677d3c3300e378157805955204ebed3d9fb188d
Danil <visdanil97@gmail.com>
2022-07-12 02:42:04 +0700

    hw1_4

---
+++ 1_Lesson/Homework/hw1_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.WriteLine("Write a natural number:");
+int N = int.Parse(Console.ReadLine());
+int i = 2;
+
+if (N > 1)
+{
+    while (i <= N)
+    {
+        Console.WriteLine(i);
+        i = i + 2;
+    }
+}
+else Console.WriteLine("There's no even natural numbers");
