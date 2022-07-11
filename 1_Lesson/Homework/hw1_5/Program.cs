77cd23d80c6426d6510653bc5f4524ca43d33be8
Danil <visdanil97@gmail.com>
2022-07-12 03:10:25 +0700

    hw1_5

---
+++ 1_Lesson/Homework/hw1_5/Program.cs
@@ -0,0 +1,8 @@
+﻿// Задача 7: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает последнюю цифру этого числа.
+
+Console.WriteLine("Write a three-digit number:");
+int a = int.Parse(Console.ReadLine());
+
+if(a<100) Console.WriteLine("Wrong number");
+else if(a>999) Console.WriteLine("Wrong number");
+else Console.WriteLine(a % 10);
