34ebbec3f817521387028cfb9d72c40c38855e96
Danil <visdanil97@gmail.com>
2022-07-15 04:05:14 +0700

    Task hw2_2

---
+++ 2_Lesson/2_Homework/hw2_2/Program.cs
@@ -0,0 +1,11 @@
+﻿//Задача 2: Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+void DeleteSecond(int number)
+{
+    Console.WriteLine(number / 100 * 10 + number % 10);
+}
+
+int a = new Random().Next(100, 1000);
+
+Console.WriteLine(a);
+DeleteSecond(a);
