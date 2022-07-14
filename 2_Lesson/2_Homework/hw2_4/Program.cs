364eb59f7a9591f74bf8b8c5de3d8228d88020b5
Danil <visdanil97@gmail.com>
2022-07-15 04:57:19 +0700

    Task hw2_4

---
+++ 2_Lesson/2_Homework/hw2_4/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void Weekend (int number)
+{
+    if (number == 6 || number == 7)
+    Console.WriteLine("It's the weekend");
+    else
+    Console.WriteLine("It's not the weekend");
+}
+
+
+Console.WriteLine("Write a number of the day in a week (from 1 to 7):");
+int a =int.Parse(Console.ReadLine());
+
+Weekend(a);
