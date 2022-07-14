7c0954d45d7cbcd139f20b7d3712af58cfb378c9
Danil <visdanil97@gmail.com>
2022-07-15 03:42:44 +0700

    Task hw2_1

---
+++ 2_Lesson/2_Homework/hw2_1/Program.cs
@@ -0,0 +1,12 @@
+﻿//Задача 1: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+void SecondNum(int number)
+{
+    int s_n = number / 10 - number / 100 * 10;
+    Console.WriteLine($"The second number of {number} is {s_n}");
+}
+
+Console.WriteLine("Write a three-digit number:");
+int a =int.Parse(Console.ReadLine());
+
+SecondNum(a);
