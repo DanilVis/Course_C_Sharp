73f96f49a3d77e07bc4a3c01b2ac3d1dfb9c35d2
Danil <visdanil97@gmail.com>
2022-07-21 00:27:13 +0700

    Task hw3_1

---
+++ 3_Lesson/3_Homework/hw3_1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+void Palindrome(int a)
+{
+    if (a / 10000 == a % 10 && a / 1000 % 10 == a / 10 % 10)
+        Console.WriteLine("The number is a palindrome");
+    else
+        Console.WriteLine("The number is not a palindrome");
+}
+
+int p = int.Parse(Console.ReadLine());
+Palindrome(p);
