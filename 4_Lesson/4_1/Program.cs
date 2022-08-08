ed1832dbc92e4191357fcb9ddd9947c1bf863e0d
Danil <visdanil97@gmail.com>
2022-08-08 23:07:55 +0700

    Task 4_1

---
+++ 4_Lesson/4_1/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 1: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+
+int NumCount(int a)
+{
+    string number = a.ToString();
+    return number.Length;
+}
+
+Console.WriteLine("Write a number: ");
+int x = int.Parse(Console.ReadLine());
+
+Console.WriteLine(NumCount(x));
