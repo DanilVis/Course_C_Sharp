3f6d6810865e6c7a7e36130a3449e51c72cd6bb2
Danil <visdanil97@gmail.com>
2022-08-09 01:29:34 +0700

    Task hw4_3

---
+++ 4_Lesson/4_Homework/hw4_3/Program.cs
@@ -0,0 +1,27 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] EightMass()
+{
+    int[] arr = new int[8];
+
+    for (int i = 0; i < 8; i++)
+    {
+        arr[i] = new Random().Next(1, 100);
+    }
+    return arr;
+}
+
+void PrintArray(int[] arr)
+{
+    int count = arr.Length;
+
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+
+int[] arr1 = EightMass();
+PrintArray(arr1);
