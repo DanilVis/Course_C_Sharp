dd5da83f6b304ee99ca3cf6ac3b65353b3fd7082
Danil <visdanil97@gmail.com>
2022-08-11 02:43:50 +0700

    Task hw5_1

---
+++ 5_Lesson/5_homework/hw5_1/Program.cs
@@ -0,0 +1,41 @@
+﻿// Задача 1: Задайте массив заполненный случайными положительными трёхзначными числами. 
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+
+void Print(int[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int[] MassNums(int size)
+{
+    int[] arr = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = new Random().Next(100, 1000);
+    }
+    return arr;
+}
+
+void EvenNum(int[] arr)
+{
+    int count = 0;
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] % 2 == 0)
+            count++;
+    }
+    Console.WriteLine($"Even numbers: {count}");
+}
+
+
+int[] arr1 = MassNums(20);
+Print(arr1);
+EvenNum(arr1);
