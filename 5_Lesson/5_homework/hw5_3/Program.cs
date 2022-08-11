01f49287832188eef6b5e04edf7f3023a2ca45be
Danil <visdanil97@gmail.com>
2022-08-11 19:21:37 +0700

    Task hw5_3

---
+++ 5_Lesson/5_homework/hw5_3/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 3: Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+void Print(double[] arr)
+{
+    int size = arr.Length;
+
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+    Console.WriteLine();
+}
+
+double[] MassNums(int size)
+{
+    double[] arr = new double[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = Math.Round(new Random().NextDouble() * 10, 2);
+    }
+    return arr;
+}
+
+double MinMaxDiff(double[] arr)
+{
+    double min = arr[0];
+    double max = arr[0];
+
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] < min)
+            min = arr[i];
+        else if (arr[i] > max)
+            max = arr[i];
+    }
+
+    return max - min;
+}
+
+double[] arr1 = MassNums(6);
+Print(arr1);
+Console.WriteLine(MinMaxDiff(arr1));
