1ba6ded8a570034ba38304fc33b609b9d84d63ec
Danil <visdanil97@gmail.com>
2022-08-12 19:31:36 +0700

    Task hw6_1

--- 6_Lesson/6_homework/hw6_1/Program.cs
+++ 6_Lesson/6_homework/hw6_1/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 1: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+int[] FillArray(int num)
+{
+    int[] arr = new int[num];
+
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = int.Parse(Console.ReadLine());
+    }
+
+    return arr;
+}
+
+void Positive(int[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+    Console.WriteLine($"You entered {count} number(s) greater than zero");
+}
+
+Console.WriteLine("Enter a number of elements: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Enter numbers one by one");
+int[] arr1 = FillArray(a);
+Positive(arr1);
