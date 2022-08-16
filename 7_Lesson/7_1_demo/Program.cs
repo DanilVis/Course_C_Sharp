abd8275706fdabad0512989b0649801a05ccbb5a
Danil <visdanil97@gmail.com>
2022-08-16 22:45:39 +0700

    task 7_1 demo

---
+++ 7_Lesson/7_1_demo/Program.cs
@@ -0,0 +1,38 @@
+﻿// 1. Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+void PrintArray(int[,] arr)
+{
+    int rowSize = arr.GetLength(0);
+    int columnSize = arr.GetLength(1);
+
+    for (int i = 0; i < rowSize; i++)
+    {
+        for (int j = 0; j < columnSize; j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArray(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(from, to);
+        }
+    }
+    return arr;
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr1 = FillArray(row, column, 1, 100);
+PrintArray(arr1);
