ea5e12f5836308ce1faebe71722e27a4ef2f2dfd
Danil <visdanil97@gmail.com>
2022-08-16 23:54:38 +0700

    task 7_4

---
+++ 7_Lesson/7_4/Program.cs
@@ -0,0 +1,56 @@
+﻿// 4. Задайте двумерный массив. Введите элемент, и найдите первое его вхождение,
+// выведите позиции по горизонтали и вертикали, или напишите, что такого элемента нет.
+
+void PrintArray(int[,] arr)
+{
+    int rowSize = arr.GetLength(0);
+    int columnSize = arr.GetLength(1);
+
+    for (int i = 0; i < rowSize; i++)
+    {
+        for (int j = 0; j < columnSize; j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArray(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(from, to);
+        }
+    }
+    return arr;
+}
+
+string FirstExposition (int [,] arr, int a)
+{
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+for (int j = 0; j < arr.GetLength(1); j++)
+{
+if (arr[i, j] == a)
+return $"({i + 1}, {j + 1})";
+}
+}
+return "No";
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr1 = FillArray(row, column, 1, 10);
+
+Console.Write("Enter a number: ");
+int num = int.Parse(Console.ReadLine());
+PrintArray(arr1);
+Console.WriteLine(FirstExposition(arr1, num));
