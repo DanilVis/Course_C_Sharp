73369425150b020901677cecee6a307a531a1316
Danil <visdanil97@gmail.com>
2022-08-17 01:54:54 +0700

    task hw7_3

---
+++ 7_Lesson/7_homework/hw7_3/Program.cs
@@ -0,0 +1,58 @@
+﻿// Задача 52. Задайте двумерный массив из целых чисел. 
+// Найдите среднее арифметическое элементов в каждом столбце.
+
+void PrintArray(int[,] arr)
+{
+    int rowSize = arr.GetLength(0);
+    int columnSize = arr.GetLength(1);
+
+    for (int i = 0; i < rowSize; i++)
+    {
+        for (int j = 0; j < columnSize; j++)
+        {
+            Console.Write($"{arr[i, j],3} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArray(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(from, to);
+        }
+    }
+    return arr;
+}
+
+void AverageNum(int[,] arr)
+{
+    int row = arr.GetLength(0);
+    int column = arr.GetLength(1);
+
+    for (int j = 0; j < column; j++)
+    {
+        double sum = 0;
+
+        for (int i = 0; i < row; i++)
+        {
+            sum += arr[i, j];
+        }
+        Console.WriteLine($"Column {j + 1} => {sum / row}");
+    }
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr1 = FillArray(row, column, 1, 10);
+PrintArray(arr1);
+Console.WriteLine();
+AverageNum(arr1);
