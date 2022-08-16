2ba8acfab5bb94f3f3acef312e031123c6d915a9
Danil <visdanil97@gmail.com>
2022-08-16 23:14:10 +0700

    task 7_2

---
+++ 7_Lesson/7_2/Program.cs
@@ -0,0 +1,54 @@
+﻿// 2. Задайте двумерный массив. Найдите элементы,
+// у которых обе позиции чётные, и замените эти элементы на их квадраты.
+
+void PrintArray(int[,] arr)
+{
+    int rowSize = arr.GetLength(0);
+    int columnSize = arr.GetLength(1);
+
+    for (int i = 0; i < rowSize; i++)
+    {
+        for (int j = 0; j < columnSize; j++)
+        {
+            Console.Write($"{arr[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArray(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(from, to);
+        }
+    }
+    return arr;
+}
+
+int[,] EvenPositionSquare(int[,] arr)
+{
+    for (int i = 1; i < arr.GetLength(0); i += 2)
+    {
+        for (int j = 1; j < arr.GetLength(1); j += 2)
+        {
+            arr[i, j] *= arr[i, j];
+        }
+    }
+    return arr;
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr1 = FillArray(row, column, 1, 10);
+PrintArray(arr1);
+Console.WriteLine();
+int[,] arr2 = EvenPositionSquare(arr1);
+PrintArray(arr2);
