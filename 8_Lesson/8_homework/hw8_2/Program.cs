529e509362bcfde077d704c1878aa395756b99dc
Danil <visdanil97@gmail.com>
2022-08-18 00:33:20 +0700

    task hw8_2

---
+++ 8_Lesson/8_homework/hw8_2/Program.cs
@@ -0,0 +1,62 @@
+﻿// Задача 2: Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+void PrintArray(int[,] arr)
+{
+    int rowSize = arr.GetLength(0);
+    int columnSize = arr.GetLength(1);
+
+    for (int i = 0; i < rowSize; i++)
+    {
+        for (int j = 0; j < columnSize; j++)
+        {
+            Console.Write($"{arr[i, j],3} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArray(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(from, to);
+        }
+    }
+    return arr;
+}
+
+void MinSumRow(int[,] arr)
+{
+    int rowWithMinSum = 0;
+    int minSum = 0;
+
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        int sum = 0;
+
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            sum += arr[i, j];
+        }
+        if (i == 0 || sum < minSum)
+        {
+            minSum = sum;
+            rowWithMinSum = i + 1;
+        }
+    }
+Console.WriteLine($"Минимальная сумма у элементов {rowWithMinSum} строки");
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr1 = FillArray(row, column, -9, 10);
+PrintArray(arr1);
+MinSumRow(arr1);
