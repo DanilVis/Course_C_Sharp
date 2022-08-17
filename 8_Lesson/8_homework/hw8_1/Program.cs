9a3306239ab3fcd4cebee3b22ae699a4613fbf6d
Danil <visdanil97@gmail.com>
2022-08-17 23:09:36 +0700

    task hw8_1

---
+++ 8_Lesson/8_homework/hw8_1/Program.cs
@@ -0,0 +1,65 @@
+﻿// Задача 1: Задайте двумерный массив. Напишите программу, 
+// которая упорядочит по убыванию элементы каждой строки двумерного массива.
+
+void PrintArray(int[,] arr)
+{
+    int rowSize = arr.GetLength(0);
+    int columnSize = arr.GetLength(1);
+
+    for (int i = 0; i < rowSize; i++)
+    {
+        for (int j = 0; j < columnSize; j++)
+        {
+            Console.Write($"{arr[i, j], 3} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int[,] FillArray(int row, int column, int from, int to)
+{
+    int[,] arr = new int[row, column];
+
+    for (int i = 0; i < row; i++)
+    {
+        for (int j = 0; j < column; j++)
+        {
+            arr[i, j] = new Random().Next(from, to);
+        }
+    }
+    return arr;
+}
+
+void SortMaxToMin(int[,] arr)
+{
+    int rowSize = arr.GetLength(0);
+    int columnSize = arr.GetLength(1);
+
+    for (int i = 0; i < rowSize; i++)
+    {
+        for (int j = 0; j < columnSize - 1; j++)
+        {
+            int maxPosition = j;
+
+            for(int k = j + 1; k < columnSize; k++)
+            {
+                if(arr[i,k] > arr[i, maxPosition])
+                maxPosition = k;
+            }
+            (arr[i,j], arr[i, maxPosition]) = (arr[i, maxPosition], arr[i,j]);
+        }
+        
+    }
+}
+
+Console.Write("Enter the number of rows: ");
+int row = int.Parse(Console.ReadLine());
+Console.Write("Enter the number of columns: ");
+int column = int.Parse(Console.ReadLine());
+
+int[,] arr1 = FillArray(row, column, 1, 100);
+PrintArray(arr1);
+
+SortMaxToMin(arr1);
+PrintArray(arr1);
