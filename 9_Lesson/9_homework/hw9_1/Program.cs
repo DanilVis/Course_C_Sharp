66d9587c345c7e7dfbeb77123421c7e2adc95d1c
Danil <visdanil97@gmail.com>
2022-08-19 16:27:25 +0700

    task hw9_2

---
+++ 9_Lesson/9_homework/hw9_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая 
+// выведет все чётные натуральные числа в промежутке от M до N.
+// M = 1; N = 5. -> ""2, 4""
+// M = 4; N = 8. -> ""4, 6, 8""
+
+void EvenNums(int min, int max)
+{
+    if (max % 2 == 1) max = max - 1;
+
+    if (max < min) return;
+    EvenNums(min, max - 2);
+    Console.Write($"{max} ");
+}
+
+EvenNums(1, 5);
+Console.WriteLine();
+EvenNums(4, 8);
+Console.WriteLine();
+EvenNums(3, 20);
